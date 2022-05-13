b2132138df7626eeb074b1cc05c603bdb88673f6
Martin Miglio <marmig0404@gmail.com>
2022-05-13 03:54:52 -0400

    add unittesting

---
+++ tests/template_store_test.py
@@ -0,0 +1,47 @@
+import unittest
+from PIL import Image
+from templatestore import TemplateStore
+import os
+
+
+class TemplateStoreTest(unittest.TestCase):
+    def test_adding_images(self):
+        # make test store
+        store_path = "tests/assets/TestStoreAdd"
+        if os.path.exists(store_path):
+            os.remove(store_path)
+        ts = TemplateStore(store_path)
+        # define template dict and open images
+        tns = ["big_building", "big_tree", "cathedral", "fireworks", "hdr", ]
+        td = {tn: Image.open("tests/assets/"+tn+".png") for tn in tns}
+        # save template dict to store
+        ts.add_templates(td)
+        # read images from store and compare
+        for name in td:
+            # if image is not the same as in original dict, fail
+            self.assertTrue(td[name] == ts.get_template(name))
+        # pass if no fails
+        self.assertTrue(True)
+
+    def test_removing_images(self):
+        # make test store
+        store_path = "tests/assets/TestStoreRemove"
+        if os.path.exists(store_path):
+            os.remove(store_path)
+        ts = TemplateStore(store_path)
+        # define template dict and open images
+        tns = ["big_building", "big_tree", "cathedral", "fireworks", "hdr", ]
+        td = {tn: Image.open("tests/assets/"+tn+".png") for tn in tns}
+        # save template dict to store
+        ts.add_templates(td)
+        # remove template from store and list
+        ts.remove_template("cathedral")
+        td.pop("cathedral")
+        ts.remove_template("hdr")
+        td.pop("hdr")
+        # read images from store and compare
+        for name in td:
+            # if image is not the same as in original dict, fail
+            self.assertTrue(td[name] == ts.get_template(name))
+        # pass if no fails
+        self.assertTrue(True)
