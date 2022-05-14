4a560ee70899b714fec03645f89d9c11b9de8fd8
Martin Miglio <marmig0404@gmail.com>
2022-05-13 20:08:52 -0400

    tag v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name='template-storage',
-    version='0.1.2',
+    version='0.1.3',
     description='A module that compresses template images into a store.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
